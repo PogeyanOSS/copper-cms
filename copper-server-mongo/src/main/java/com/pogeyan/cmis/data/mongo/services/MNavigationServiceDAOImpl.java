a94af37edc7918f818498a7dd50ca8361b02d0df
azarm.pogeyan <azar.m@pogeyan.com>
2017-12-13 17:41:51 +0530

    Adding method description

--- copper-server-mongo/src/main/java/com/pogeyan/cmis/data/mongo/services/MNavigationServiceDAOImpl.java
+++ copper-server-mongo/src/main/java/com/pogeyan/cmis/data/mongo/services/MNavigationServiceDAOImpl.java
@@ -43,10 +43,18 @@ public class MNavigationServiceDAOImpl extends BasicDAO<MBaseObject, ObjectId> i
 	}
 
 	/*
-	 * (non-Javadoc) filterExpression supports eq, le, ge, gt, le, lt,
-	 * startswith, endswith example filter:
-	 * "properties.orderId eq '100' and properties.orderName eq 'exampleData' or startswith(name, 'fd')"
-	 * example order: "name asc, repositoryId"
+	 * (non-Javadoc) filterExpression supports eq, ne, ge, gt, le, lt,
+	 * startswith, endswith. example filter:
+	 * "properties.orderId eq 100 and name eq pogeyan or startswith (name::'a')"
+	 * "*,modifiedAt le 123456789 and typeId eq cmis:folder" -->* represents to
+	 * get all properties data in that object
+	 * "properties.isRead eq false and typeId ne cmis:folder"
+	 * "properties.orderId gt 100 properties.purchaseOrder ge 100"
+	 * "startswith (name::'a') and properties.orderId lt 100"
+	 * "properties.orderId le 100"
+	 * 
+	 * example order: "name asc, repositoryId" 
+	 * "name desc"
 	 * 
 	 * @see
 	 * com.pogeyan.cmis.api.data.services.MNavigationServiceDAO#getChildren(java
@@ -114,6 +122,26 @@ public class MNavigationServiceDAOImpl extends BasicDAO<MBaseObject, ObjectId> i
 		}
 	}
 
+	/*
+	 * (non-Javadoc) filterExpression supports eq, ne, ge, gt, le, lt,
+	 * startswith, endswith. 
+	 * example filter:
+	 * "properties.orderId eq 100 and name eq pogeyan or startswith (name::'a')"
+	 * "*,modifiedAt le 123456789 and typeId eq cmis:folder" -->* represents to
+	 * get all properties data in that object
+	 * "properties.isRead eq false and typeId ne cmis:folder"
+	 * "properties.orderId gt 100 properties.purchaseOrder ge 100"
+	 * "startswith (name::'a') and properties.orderId lt 100"
+	 * "properties.orderId le 100"
+	 * 
+	 * example order: "name asc, repositoryId",
+	 * "name desc"
+	 * 
+	 * @see
+	 * com.pogeyan.cmis.api.data.services.MNavigationServiceDAO#getDescendants(
+	 * java .lang.String, java.lang.String[], boolean,java.lang.String[],
+	 * java.lang.String)
+	 */
 	@SuppressWarnings("unchecked")
 	@Override
 	public List<MBaseObject> getDescendants(String path, String[] principalIds, boolean aclPropagation,
