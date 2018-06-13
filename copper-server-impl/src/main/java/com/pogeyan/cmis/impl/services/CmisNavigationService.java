1b2cf05362c5e9d27417bcf827bd3442c15c15d2
azarm.pogeyan <azar.m@pogeyan.com>
2018-06-13 14:56:30 +0530

    remove sending empty check children

--- copper-server-impl/src/main/java/com/pogeyan/cmis/impl/services/CmisNavigationService.java
+++ copper-server-impl/src/main/java/com/pogeyan/cmis/impl/services/CmisNavigationService.java
@@ -447,10 +447,6 @@ public class CmisNavigationService {
 					}
 					childrenOfFolderId.add(oifc);
 				}
-			} else {
-				ObjectInFolderContainerImpl oifc = new ObjectInFolderContainerImpl();
-				oifc.setChildren(new ArrayList<ObjectInFolderContainer>());
-				childrenOfFolderId.add(oifc);
 			}
 			return childrenOfFolderId;
 		}
