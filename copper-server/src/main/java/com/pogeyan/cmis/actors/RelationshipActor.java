0dec73ce07877350df636a76267a950e4428aecd
Swathi Susi S <swathisusi.s@pogeyan.com>
2018-10-31 09:45:29 +0530

    tracing for RelationshipActor

--- copper-server/src/main/java/com/pogeyan/cmis/actors/RelationshipActor.java
+++ copper-server/src/main/java/com/pogeyan/cmis/actors/RelationshipActor.java
@@ -16,6 +16,8 @@
 package com.pogeyan.cmis.actors;
 
 import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 import org.apache.chemistry.opencmis.commons.data.ObjectList;
@@ -32,10 +34,13 @@ import com.mongodb.MongoException;
 import com.pogeyan.cmis.api.BaseClusterActor;
 import com.pogeyan.cmis.api.BaseRequest;
 import com.pogeyan.cmis.api.BaseResponse;
+import com.pogeyan.cmis.api.data.ISpan;
 import com.pogeyan.cmis.api.messages.CmisBaseResponse;
 import com.pogeyan.cmis.api.messages.QueryGetRequest;
 import com.pogeyan.cmis.api.utils.Helpers;
+import com.pogeyan.cmis.browser.BrowserConstants;
 import com.pogeyan.cmis.impl.services.CmisRelationshipService;
+import com.pogeyan.cmis.tracing.TracingApiServiceFactory;
 
 public class RelationshipActor extends BaseClusterActor<BaseRequest, BaseResponse> {
 	private static final Logger LOG = LoggerFactory.getLogger(RelationshipActor.class);
@@ -46,16 +51,25 @@ public class RelationshipActor extends BaseClusterActor<BaseRequest, BaseRespons
 	}
 
 	public RelationshipActor() {
-		this.registerMessageHandle("relationships", QueryGetRequest.class, (t, b) -> CompletableFuture.supplyAsync(
-				() -> CmisBaseResponse.fromWithTryCatch(() -> this.getRelationships((QueryGetRequest) t))));
-
+		this.registerMessageHandle("relationships", QueryGetRequest.class,
+				(t, b) -> CompletableFuture.supplyAsync(() -> CmisBaseResponse.fromWithTryCatch(
+						() -> this.getRelationships((QueryGetRequest) t, (HashMap<String, Object>) b))));
 	}
 
-	private JSONObject getRelationships(QueryGetRequest request)
+	private JSONObject getRelationships(QueryGetRequest request, HashMap<String, Object> baggage)
 			throws CmisObjectNotFoundException, MongoException, CmisRuntimeException {
+		String tracingId = (String) baggage.get(BrowserConstants.TRACINGID);
+		ISpan parentSpan = (ISpan) baggage.get(BrowserConstants.PARENT_SPAN);
+		ISpan span = TracingApiServiceFactory.getApiService().startSpan(tracingId, parentSpan,
+				"RelationshipActor::getRelationships", null);
 		String permission = request.getUserObject().getPermission();
+		Map<String, Object> attrMap = new HashMap<String, Object>();
 		if (!Helpers.checkingUserPremission(permission, "get")) {
-			throw new CmisRuntimeException(request.getUserName() + " is not authorized to applyAcl.");
+			attrMap.put("error", request.getUserName() + "is not authorized, TraceId:" + span.getTraceId());
+			TracingApiServiceFactory.getApiService().updateSpan(span, true,
+					request.getUserName() + " is not authorized", attrMap);
+			TracingApiServiceFactory.getApiService().endSpan(tracingId, span);
+			throw new CmisRuntimeException(request.getUserName() + " is not authorized.");
 		}
 		String objectId = request.getObjectId();
 		Boolean includeSubRelationshipTypes = request.getBooleanParameter(QueryGetRequest.PARAM_SUB_RELATIONSHIP_TYPES);
@@ -75,14 +89,18 @@ public class RelationshipActor extends BaseClusterActor<BaseRequest, BaseRespons
 
 		ObjectList relationships = CmisRelationshipService.Impl.getObjectRelationships(request.getRepositoryId(),
 				objectId, includeSubRelationshipTypes, relationshipDirection, typeId, renditionFilter,
-				includeAllowableActions, maxItems, skipCount, null, request.getUserObject());
-
+				includeAllowableActions, maxItems, skipCount, null, request.getUserObject(), tracingId, span);
 		if (relationships == null) {
+			attrMap.put("error", "Relationships are null!, TraceId:" + span.getTraceId());
+			TracingApiServiceFactory.getApiService().updateSpan(span, true,
+					request.getUserName() + " Relationships are null!", attrMap);
+			TracingApiServiceFactory.getApiService().endSpan(tracingId, span);
 			throw new CmisRuntimeException("Relationships are null!");
 		}
 
 		JSONObject jsonChildren = JSONConverter.convert(relationships, null, JSONConverter.PropertyMode.OBJECT,
 				succinct, dateTimeFormat);
+		TracingApiServiceFactory.getApiService().endSpan(tracingId, span);
 		return jsonChildren;
 
 	}
