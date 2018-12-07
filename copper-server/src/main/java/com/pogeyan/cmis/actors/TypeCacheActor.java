65a7ac4125f976d56aea1e8e1dd6bff13cffc325
Swathi Susi S <swathisusi.s@pogeyan.com>
2018-12-07 16:01:16 +0530

    tracing for typeCacheActor

--- copper-server/src/main/java/com/pogeyan/cmis/actors/TypeCacheActor.java
+++ copper-server/src/main/java/com/pogeyan/cmis/actors/TypeCacheActor.java
@@ -1,5 +1,6 @@
 package com.pogeyan.cmis.actors;
 
+import java.util.HashMap;
 import java.util.concurrent.CompletableFuture;
 
 import org.apache.chemistry.opencmis.commons.exceptions.CmisObjectNotFoundException;
@@ -11,9 +12,12 @@ import org.slf4j.LoggerFactory;
 import com.pogeyan.cmis.api.BaseClusterActor;
 import com.pogeyan.cmis.api.BaseRequest;
 import com.pogeyan.cmis.api.BaseResponse;
+import com.pogeyan.cmis.api.data.ISpan;
 import com.pogeyan.cmis.api.messages.CmisBaseResponse;
 import com.pogeyan.cmis.api.messages.QueryGetRequest;
+import com.pogeyan.cmis.browser.BrowserConstants;
 import com.pogeyan.cmis.impl.factory.CacheProviderServiceFactory;
+import com.pogeyan.cmis.tracing.TracingApiServiceFactory;
 
 public class TypeCacheActor extends BaseClusterActor<BaseRequest, BaseResponse> {
 	private static final Logger LOG = LoggerFactory.getLogger(TypeCacheActor.class);
@@ -24,16 +28,23 @@ public class TypeCacheActor extends BaseClusterActor<BaseRequest, BaseResponse> 
 	}
 
 	public TypeCacheActor() {
-		this.registerMessageHandle("resetcache", QueryGetRequest.class, (t, b) -> CompletableFuture
-				.supplyAsync(() -> CmisBaseResponse.fromWithTryCatch(() -> this.clearCache((QueryGetRequest) t))));
+		this.registerMessageHandle("resetcache", QueryGetRequest.class,
+				(t, b) -> CompletableFuture.supplyAsync(() -> CmisBaseResponse
+						.fromWithTryCatch(() -> this.clearCache((QueryGetRequest) t, (HashMap<String, Object>) b))));
 
 	}
 
-	private JSONObject clearCache(QueryGetRequest request) throws CmisObjectNotFoundException, CmisRuntimeException {
+	private JSONObject clearCache(QueryGetRequest request, HashMap<String, Object> baggage)
+			throws CmisObjectNotFoundException, CmisRuntimeException {
+		String tracingId = (String) baggage.get(BrowserConstants.TRACINGID);
+		ISpan parentSpan = (ISpan) baggage.get(BrowserConstants.PARENT_SPAN);
+		ISpan span = TracingApiServiceFactory.getApiService().startSpan(tracingId, parentSpan,
+				"TypeCacheActor::clearCache", null);
 		LOG.info("Method name: {}, repositoryId: {}", "clearCache", request.getRepositoryId());
 		CacheProviderServiceFactory.getTypeCacheServiceProvider().removeAll(request.getRepositoryId());
 		JSONObject j = new JSONObject();
 		j.put("status", true);
+		TracingApiServiceFactory.getApiService().endSpan(tracingId, span);
 		return j;
 	}
 }
