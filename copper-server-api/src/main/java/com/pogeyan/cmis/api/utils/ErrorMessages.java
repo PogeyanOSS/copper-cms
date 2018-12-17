f4430822fd2abc995993d4a21593feb76210acb2
Swathi Susi S <swathisusi.s@pogeyan.com>
2018-12-17 12:15:54 +0530

    changes in update span messages

--- copper-server-api/src/main/java/com/pogeyan/cmis/api/utils/ErrorMessages.java
+++ copper-server-api/src/main/java/com/pogeyan/cmis/api/utils/ErrorMessages.java
@@ -11,8 +11,8 @@ public class ErrorMessages {
 	public static String ILLEGAL_EXCEPTION = "IllegalArgumentException";
 	public static String BASE_EXCEPTION = "exception";
 	public static String UNKNOWN_OBJECT = "Unknown object id: %s, TraceId: %s";
-	public static String NOT_AUTHORISED = "%s is not authorized to applyAcl, TraceId: %s";
-	public static String TOKEN_VALUE_NULL = "change log token value should not be null, TraceId: %s";
+	public static String NOT_AUTHORISED = "%s is not authorized to applyAcl";
+	public static String TOKEN_VALUE_NULL = "change log token value should not be null";
 	public static String ACL_NULL = "object acl is null, TraceId: %s";
 	public static String RELATIONSHIP_NULL = "Relationships are null, TraceId: %s";
 	public static String EXCEPTION = "exception: %s, TraceId: %s";
