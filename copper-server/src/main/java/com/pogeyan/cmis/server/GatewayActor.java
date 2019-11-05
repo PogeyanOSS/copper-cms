b79c2f98c2db6c6613119a47acbf6603a6ce7689
Praveen Kumar Konathala <praveen.k@pogeyan.com>
2019-11-05 18:43:49 +0530

    refactored the gateWay actor

--- copper-server/src/main/java/com/pogeyan/cmis/server/GatewayActor.java
+++ copper-server/src/main/java/com/pogeyan/cmis/server/GatewayActor.java
@@ -77,7 +77,8 @@ public class GatewayActor extends UntypedActor {
 				if (!this.messageRefs.containsKey(bm.getMessageId())) {
 					this.messageRefs.put(bm.getMessageId(), this.getSender());
 					// get actor either with type name or action name
-					ActorRef actorReference = ActorServiceFactory.getInstance().initActorRef(bm.getTypeName(), bm.getActionName());
+					ActorRef actorReference = ActorServiceFactory.getInstance().initActorRef(bm.getTypeName(),
+							bm.getActionName());
 					actorRefs.put(bm.getMessageId(), actorReference);
 					if (actorReference != null) {
 						// get type name from actor reference
@@ -119,9 +120,9 @@ public class GatewayActor extends UntypedActor {
 						LOG.debug("Reply back to servlet actor for message: {}", bm);
 					}
 					ActorRef responseActorRef = this.messageRefs.get(bm.getMessageId());
-					if(!ActorServiceFactory.getInstance().isSingletonActor(actorRefs.get(bm.getMessageId()))) {
-                        ActorServiceFactory.getInstance().getSystem().stop(actorRefs.get(bm.getMessageId()));
-                    }
+					if (!ActorServiceFactory.getInstance().isSingletonActor(actorRefs.get(bm.getMessageId()))) {
+						ActorServiceFactory.getInstance().getSystem().stop(actorRefs.get(bm.getMessageId()));
+					}
 					this.messageRefs.remove(bm.getMessageId());
 					responseActorRef.tell(bm, ActorRef.noSender());
 					if (Helpers.isPerfMode()) {
