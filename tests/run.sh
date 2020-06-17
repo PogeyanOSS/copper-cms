25910382217b4dc4abc5d34913edcbb01514da03
Praveen Kumar Konathala <praveen.k@pogeyan.com>
2020-06-17 15:21:54 +0530

    updating the clearDB script

--- tests/run.sh
+++ tests/run.sh
@@ -3,6 +3,12 @@ export CMIS_USERNAME=$2
 export CMIS_PASSWORD=$3
 export CMIS_REPO_ID=$4
 export HOSTURL=$5
+echo "web packing cleardb js script"
+
+npm run build
+echo "doing to git directory"
+
+cd dist/
 
 echo "clearing the db"
 
@@ -10,10 +16,12 @@ node clearDB.js
 
 echo "runing tcks tests"
 
-cd ../
+cd ../../
 
 mvn install -pl chemistry-opencmis-test-tck
 
 echo "clearing the db"
+
+cd test/dist/
 
 node clearDB.js
